7055374995713cbfd9503798722ce687ac2e4538
A Student <student@nowhere.edu>
2026-04-20 22:41:14 +0000

    Apr 20, 2026, 3:41 PM

--- tests.cpp
+++ tests.cpp
@@ -33,7 +33,7 @@ int frameBorderArea(int width, int height, int border)
 {
   int outsideArea = width * height;
   int insideWidth = width - 2 * border;
-  int insideheight = 
+  int insideheight = height 
 }
 
 ///----------------------------------------------------------------------------------
