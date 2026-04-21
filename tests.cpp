cc6bc22f9349b2e944d11d2c66a8d927a65706df
A Student <student@nowhere.edu>
2026-04-21 00:24:03 +0000

    Apr 20, 2026, 5:24 PM

--- tests.cpp
+++ tests.cpp
@@ -93,5 +93,5 @@ TEST_CASE("getMaxBorder")
 // Add your own test case for hangingWireLength here
 TEST_CASE("hangWireLength"){
   cout << "5 : hangWireLength" << endl;
-  CHECK(hangWireLength(4,3))
+  CHECK(hangWireLength(4,3) = )
 }
